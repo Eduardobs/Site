2bb7653e048f8d73e982ac026de9ebbdcfe727a9
Eduardobs <ebs.csp@gmail.com>
2018-09-05 08:29:57 -0300

    Adicionadas algumas validações JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-﻿function validarUsuarioSenha(formulario) {
-    if (formulario.email == "")
+﻿function validarUsuarioSenhaX(formulario) {
+    if (formulario.email == "") {
+        formulario.email.focus();
+        alert("E-mail não informado!");
+        return false;
+    }
 
+    if (formulario.senha=="") {
+        formulario.senha.focus();
+        alert("Senha não informada!");
+        return false;
+    }
+    return true;
 }
